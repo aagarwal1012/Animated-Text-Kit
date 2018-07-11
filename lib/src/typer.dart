d23815313bcd33669b94f3f83b7b9c98041c8f5b
Ayush Agarwal <aagarwal9782@gmail.com>
2018-07-12 00:39:42 +0530

    Updated Typer : code clean up.

--- lib/src/typer.dart
+++ lib/src/typer.dart
@@ -20,17 +20,11 @@ class Typer extends StatefulWidget {
 class _TyperState extends State<Typer> with SingleTickerProviderStateMixin {
 
   AnimationController _controller;
-//  Animation<int> _animation;
 
   List<Animation<int>> _typingText = [];
-//  List<Animation<double>> _fadeIn = [];
   List<Animation<double>> _fadeOut = [];
 
   List<Widget> textWidgetList = [];
-
-//  int _stringIndexNumber = 0;
-
-//  String get _currentString => widget.text[_stringIndexNumber];
 
   @override
   void initState() {
@@ -41,62 +35,6 @@ class _TyperState extends State<Typer> with SingleTickerProviderStateMixin {
       vsync: this,
     )
       ..repeat();
-
-//    _controller
-//    ..addStatusListener((status) {
-//      if(status == AnimationStatus.completed){
-//        setState(() {
-//          _stringIndexNumber = (_stringIndexNumber + 1) % widget.text.length;
-//          _animation = StepTween(
-//              begin: 0, end: _currentString.length).animate(
-//              new CurvedAnimation(
-//                  parent: _controller,
-//                  curve: Interval(0.0, 0.9, curve: Curves.linear)
-//              )
-//          );
-//        });
-////      _controller.forward();
-//      }
-//    });
-
-//      ..addListener(() {
-//        if (_controller.status == AnimationStatus.completed) {
-//          setState(() {
-//            _stringIndexNumber = (_stringIndexNumber + 1) % widget.text.length;
-//            _animation = StepTween(
-//                begin: 0, end: _currentString.length).animate(
-//                new CurvedAnimation(
-//                    parent: _controller,
-//                    curve: Interval(0.0, 1.0, curve: Curves.linear)
-//                )
-//            )
-//              ..addListener(() => setState(() => {}));
-//          });
-//          _controller.forward();
-//        }
-//        if (_controller.status == AnimationStatus.dismissed) {
-//          setState(() {
-//            _animation = StepTween(
-//                begin: 0, end: _currentString.length).animate(
-//                new CurvedAnimation(
-//                    parent: _controller,
-//                    curve: Interval(0.0, 0.9, curve: Curves.linear)
-//                )
-//            )
-//              ..addListener(() => setState(() => {}));
-//          });
-//          _controller.forward();
-//        }
-//      });
-
-//    _animation = StepTween(
-//        begin: 0, end: _currentString.length).animate(
-//        new CurvedAnimation(
-//            parent: _controller,
-//            curve: Interval(0.0, 0.9, curve: Curves.linear)
-//        )
-//    )
-//      ..addListener(() => setState(() => {}));
 
     int totalCharacters = 0;
 
@@ -120,19 +58,6 @@ class _TyperState extends State<Typer> with SingleTickerProviderStateMixin {
           )
             ..addListener(() => setState(() => {}))
       );
-//
-//      _fadeIn.add(
-//          Tween(
-//              begin: 0.0, end: 1.0).animate(
-//              new CurvedAnimation(
-//                  parent: _controller,
-//                  curve: Interval(
-//                      percentTimeCount, (percentTimeCount + (percentTime / 10)),
-//                      curve: Curves.easeOut)
-//              )
-//          )
-//            ..addListener(() => setState(() => {}))
-//      );
 
       _fadeOut.add(
           Tween(
@@ -148,10 +73,6 @@ class _TyperState extends State<Typer> with SingleTickerProviderStateMixin {
 
       percentTimeCount += percentTime;
     }
-
-
-
-//    _controller.forward();
   }
 
 
@@ -163,50 +84,25 @@ class _TyperState extends State<Typer> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-
     for (int i = 0; i < widget.text.length; i++) {
       textWidgetList.add(
           AnimatedBuilder(
-              animation: _controller,
-              builder: (BuildContext context, Widget child){
-                return Opacity(
-//                  opacity: (_typingText[i].value == widget.text.length) ? _fadeOut[i].value : _fadeIn[i].value,
-                  opacity: _fadeOut[i].value,
-                    child: Text(
-                    widget.text[i].substring(0, _typingText[i].value),
-                    style: widget.textStyle,
-                  ),
-                );
-              },
+            animation: _controller,
+            builder: (BuildContext context, Widget child) {
+              return Opacity(
+                opacity: _fadeOut[i].value,
+                child: Text(
+                  widget.text[i].substring(0, _typingText[i].value),
+                  style: widget.textStyle,
+                ),
+              );
+            },
           )
       );
     }
 
-//    String showText = _currentString.substring(
-//        0, _animation.value);
-//    return AnimatedBuilder(
-//      animation: _controller,
-//      builder: (BuildContext context, Widget child) {
-////          return Text(
-////            _currentString.substring(0, _animation.value),
-////            style: widget.textStyle,
-////          );
-//      return child;
-//      },
-//      child: Text(
-//        _currentString.substring(0, _animation.value),
-//        style: widget.textStyle,
-//      ),
-//  );
-//
-//    return Text(
-//      _currentString.substring(0, _animation.value),
-//      style: widget.textStyle,
-//    );
-
     return Stack(
       children: textWidgetList,
     );
-
   }
 }
