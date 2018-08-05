d79009044e6ba126ec3ad78d1fcaf404f0fe2064
aagarwal1012 <aagarwal9782@gmail.com>
2018-08-05 23:42:37 +0530

    Typer : removed addlisteners for animations

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -47,8 +47,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: new Center(
         child: Typer(
-          text: ["Ayush Agarwal", "Superman", "Batman"],
-          duration: Duration(milliseconds: 10000),
+          text: ["Antman", "Superman", "Batman"],
+          duration: Duration(milliseconds: 5000),
           textStyle: TextStyle(fontSize: 24.0, color: Colors.black),
         ),
       ),
