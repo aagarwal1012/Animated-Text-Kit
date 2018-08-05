d21230848846617365542202f05c9d2e3e2202f4
aagarwal1012 <aagarwal9782@gmail.com>
2018-08-05 23:47:56 +0530

    Rotating text : removed animation listeners and code cleanup

--- lib/src/rotating_text.dart
+++ lib/src/rotating_text.dart
@@ -42,33 +42,10 @@ class _RotatingTextState extends State<RotatingText>
     int lengthList = widget.text.length;
 
     double percentTime = 1.0 / lengthList;
-    double fadeTime = 1.0 / (lengthList * 5);
+    double fadeTime = 1.0 / (lengthList * 7);
     double slideTime = 1.0 / (lengthList * 3);
 
     for (int i = 0; i < widget.text.length; i++) {
-//      if (i == 0) {
-//        _fadeOut.add(
-//            Tween<double>(begin: 1.0, end: 0.0)
-//                .animate(
-//                CurvedAnimation(parent: _controller,
-//                    curve: Interval((percentTime - fadeTime), percentTime,
-//                        curve: Curves.easeIn))
-//            )
-//        );
-//        _slideOut.add(
-//            new AlignmentTween(
-//                begin: new Alignment(-1.0, 0.0), end: new Alignment(-1.0, 1.0))
-//                .animate(
-//                CurvedAnimation(
-//                    parent: _controller,
-//                    curve: Interval(
-//                        (percentTime - slideTime), percentTime,
-//                        curve: Curves.easeIn)))
-//              ..addListener(() => setState(() => {})
-//              )
-//        );
-//      }
-//      else if (i < widget.text.length - 1) {
       if (i == 0) {
         _slideIn.add(
             AlignmentTween(
@@ -82,7 +59,6 @@ class _RotatingTextState extends State<RotatingText>
                         curve: Curves.easeIn)
                 )
             )
-              ..addListener(() => setState(() => {}))
         );
         _fadeIn.add(
             Tween<double>(begin: 0.0, end: 1.0)
@@ -110,7 +86,6 @@ class _RotatingTextState extends State<RotatingText>
                         curve: Curves.easeIn)
                 )
             )
-              ..addListener(() => setState(() => {}))
         );
         _fadeIn.add(
             Tween<double>(begin: 0.0, end: 1.0)
@@ -137,7 +112,6 @@ class _RotatingTextState extends State<RotatingText>
                       curve: Curves.easeIn)
               )
           )
-            ..addListener(() => setState(() => {}))
       );
       _fadeOut.add(
           Tween<double>(begin: 1.0, end: 0.0)
@@ -151,35 +125,6 @@ class _RotatingTextState extends State<RotatingText>
               )
           )
       );
-//      }
-//      else {
-//        _slideIn.add(
-//            AlignmentTween(
-//              begin: Alignment(-1.0, -1.0), end: new Alignment(-1.0, 0.0),
-//            ).animate(
-//                CurvedAnimation(
-//                    parent: _controller,
-//                    curve: Interval(
-//                        (i * percentTime) - slideTime,
-//                        (i) * percentTime,
-//                        curve: Curves.easeIn)
-//                )
-//            )
-//              ..addListener(() => setState(() => {}))
-//        );
-//        _fadeIn.add(
-//            Tween<double>(begin: 0.0, end: 1.0)
-//                .animate(
-//                CurvedAnimation(parent: _controller,
-//                    curve: Interval(
-//                        (i * percentTime) - slideTime,
-//                        (i * percentTime) - slideTime + fadeTime,
-//                        curve: Curves.easeOut
-//                    )
-//                )
-//            )
-//        );
-//      }
     }
   }
 
@@ -193,81 +138,27 @@ class _RotatingTextState extends State<RotatingText>
   @override
   Widget build(BuildContext context) {
     for (int i = 0; i < widget.text.length; i++) {
-//      if (i == 0) {
-//        textWidgetList.add(
-//            AnimatedBuilder(
-//              animation: _controller,
-//              builder: (BuildContext context, Widget child){
-//                return Align(
-//                  alignment: _slideOut[i].value,
-//                  child: Opacity(
-//                    opacity: _fadeOut[i].value,
-//                    child: Text(
-//                      widget.text[i],
-//                      style: widget.textStyle,
-//                    ),
-//                  ),
-//                );
-//              },
-//            )
-//        );
-//      }
-//      else if (i < widget.text.length - 1) {
       textWidgetList.add(
-            AnimatedBuilder(
-              animation: _controller,
-              builder: (BuildContext context, Widget child){
-                return AlignTransition(
-                  alignment: !(_slideIn[i].value.y == 0.0)
-                      ? _slideIn[i]
-                      : _slideOut[i],
-                  child: Opacity(
-                    opacity: !(_fadeIn[i].value == 1.0)
-                        ? _fadeIn[i].value
-                        : _fadeOut[i].value,
-                    child: Text(
-                      widget.text[i],
-                      style: widget.textStyle,
-                    ),
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (BuildContext context, Widget child) {
+              return AlignTransition(
+                alignment: !(_slideIn[i].value.y == 0.0)
+                    ? _slideIn[i]
+                    : _slideOut[i],
+                child: Opacity(
+                  opacity: !(_fadeIn[i].value == 1.0)
+                      ? _fadeIn[i].value
+                      : _fadeOut[i].value,
+                  child: Text(
+                    widget.text[i],
+                    style: widget.textStyle,
                   ),
-                );
-              },
-            )
-//          AlignTransition(
-//            alignment: !(_slideIn[i].value.y == 0.0)
-//                ? _slideIn[i]
-//                : _slideOut[i],
-//            child: FadeTransition(
-//              opacity: !(_fadeIn[i].value == 1.0)
-//                  ? _fadeIn[i]
-//                  : _fadeOut[i],
-//              child: Text(
-//                widget.text[i],
-//                style: widget.textStyle,
-//              ),
-//            ),
-//          )
+                ),
+              );
+            },
+          )
       );
-//      }
-//      else {
-//        textWidgetList.add(
-//            AnimatedBuilder(
-//              animation: _controller,
-//              builder: (BuildContext context, Widget child){
-//                return Align(
-//                  alignment: _slideIn[i - 1].value,
-//                  child: Opacity(
-//                    opacity: _fadeIn[i - 1].value,
-//                    child: Text(
-//                      widget.text[i],
-//                      style: widget.textStyle,
-//                    ),
-//                  ),
-//                );
-//              },
-//            )
-//        );
-//      }
     }
 
     return SizedBox(
