c732a9d621799f65ec00acc5b1889d3ab8bd2cf0
aagarwal1012 <aagarwal9782@gmail.com>
2018-08-09 23:47:58 +0530

    added smoke test {not working}

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,29 +1,41 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Check button smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text(labels[0]), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text(labels[1]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[2]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[3]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[4]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[5]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
   });
 }
