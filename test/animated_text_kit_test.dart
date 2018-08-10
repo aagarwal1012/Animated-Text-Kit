01299c2406792f6b16056493de658cf6df9f0450
aagarwal1012 <aagarwal9782@gmail.com>
2018-08-10 15:10:03 +0530

    initial release 1.0.1

--- test/animated_text_kit_test.dart
+++ test/animated_text_kit_test.dart
@@ -1,5 +1,42 @@
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../example/lib/main.dart';
 
 void main() {
-  test('adds one to input values', () {});
+
+  testWidgets('Check button smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new MyApp());
+
+    expect(find.text(labels[0]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[1]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[2]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[3]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[4]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+
+    expect(find.text(labels[5]), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+  });
+
 }
