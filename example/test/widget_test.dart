c7d363deab39f8206fc1b3d2b7bb3c15b2a78797
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2019-12-22 00:41:22 +0530

    resolving codecov issue

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,1 +1,25 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+void main() {
+
+  test('default value for totalRepeatCount', () {
+    TypewriterAnimatedTextKit typewriter = TypewriterAnimatedTextKit(
+      onTap: () {
+        print("Tap Event");
+      },
+      text: [
+        "Discipline is the best tool",
+        "Design first, then code",
+        "Do not patch bugs out, rewrite them",
+        "Do not test bugs out, design them out",
+      ],
+      textStyle: TextStyle(fontSize: 30.0, fontFamily: "Agne"),
+    );
+
+    expect(typewriter.totalRepeatCount, 3);
+    expect(typewriter.isRepeatingAnimation, true);
+  });
+
+}
