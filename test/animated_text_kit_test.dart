a5069cc65c7e3dfd18f14a2391bceecc9c584e5b
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2019-12-22 00:30:59 +0530

    resolving codecov issue

--- test/animated_text_kit_test.dart
+++ test/animated_text_kit_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../example/lib/main.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   testWidgets('Check button smoke test', (WidgetTester tester) async {
@@ -36,5 +37,25 @@ void main() {
 
     await tester.tap(find.byIcon(Icons.play_circle_filled));
     await tester.pump();
+  });
+
+  group('TypewriterAnimatedTextKit', () {
+    test('default value for totalRepeatCount', () {
+      TypewriterAnimatedTextKit typewriter = TypewriterAnimatedTextKit(
+        onTap: () {
+          print("Tap Event");
+        },
+        text: [
+          "Discipline is the best tool",
+          "Design first, then code",
+          "Do not patch bugs out, rewrite them",
+          "Do not test bugs out, design them out",
+        ],
+        textStyle: TextStyle(fontSize: 30.0, fontFamily: "Agne"),
+      );
+
+      expect(typewriter.totalRepeatCount, 3);
+      expect(typewriter.isRepeatingAnimation, true);
+    });
   });
 }
