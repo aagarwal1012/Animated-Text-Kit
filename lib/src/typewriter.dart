fcd42a2ff6a9717efe7f07a8966fb4ee860ef9c9
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2019-12-25 17:01:47 +0530

    Enhanced different classes of AnimatedTextKit

--- lib/src/typewriter.dart
+++ lib/src/typewriter.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
 class TypewriterAnimatedTextKit extends StatefulWidget {
   /// List of [String] that would be displayed subsequently in the animation.
-  final List<String> text;
+  final List text;
 
   /// Gives [TextStyle] to the text strings.
   final TextStyle textStyle;
 
-  /// Override the [Duration] of the animation by setting the duration parameter.
+  /// The [Duration] of the delay between the apparition of each characters.
   ///
-  /// This will set the total duration for the animated widget.
-  /// For example, if text = ["a", "b", "c"] and if you want that each animation
-  /// should take 3 seconds then you have to set [duration] to 9 seconds.
-  final Duration duration;
+  /// By default it is set to 30 milliseconds
+  final Duration speed;
+
+  /// Define the [Duration] of the pause between texts
+  ///
+  /// By default it is set to 1000 milliseconds.
+  final Duration pause;
 
   /// Adds the onTap [VoidCallback] to the animated widget.
   final VoidCallback onTap;
+
+  /// Adds the onFinished [VoidCallback] to the animated widget.
+  ///
+  /// This method will run only if [isRepeatingAnimation] is set to false.
+  final VoidCallback onFinished;
+
+  /// Adds the onNext [VoidCallback] to the animated widget.
+  ///
+  /// Will be called right before the next text, after the pause parameter
+  final Function onNext;
+
+  /// Adds the onNextBeforePause [VoidCallback] to the animated widget.
+  ///
+  /// Will be called at the end of n-1 animation, before the pause parameter
+  final Function onNextBeforePause;
 
   /// Adds [AlignmentGeometry] property to the text in the widget.
   ///
@@ -27,224 +47,239 @@ class TypewriterAnimatedTextKit extends StatefulWidget {
   /// By default it is set to [TextAlign.start]
   final TextAlign textAlign;
 
+  /// Sets the number of times animation should repeat
+  ///
+  /// By default it is set to 3
+  final int totalRepeatCount;
+
   /// Set if the animation should not repeat by changing the value of it to false.
   ///
   /// By default it is set to true.
   final bool isRepeatingAnimation;
 
+  /// Should the animation ends up early and display full text if you tap on it ?
+  ///
+  /// By default it is set to false.
+  final bool displayFullTextOnTap;
+
+  /// If on pause, should a tap remove the remaining pause time ?
+  ///
+  /// By default it is set to false.
+  final bool stopPauseOnTap;
+
   TypewriterAnimatedTextKit(
       {Key key,
       @required this.text,
       this.textStyle,
-      this.duration,
+      this.speed,
+      this.pause,
+      this.displayFullTextOnTap = false,
+      this.stopPauseOnTap = false,
       this.onTap,
+      this.onNext,
+      this.onNextBeforePause,
+      this.onFinished,
+      this.totalRepeatCount = 3,
       this.alignment = AlignmentDirectional.topStart,
       this.textAlign = TextAlign.start,
       this.isRepeatingAnimation = true})
-      : super(key: key);
+      : assert(!(text == null), 'You should specify the list of text'),
+        super(key: key);
 
   @override
   _TypewriterState createState() => new _TypewriterState();
 }
 
 class _TypewriterState extends State<TypewriterAnimatedTextKit>
-    with SingleTickerProviderStateMixin {
-  Duration _duration;
-
+    with TickerProviderStateMixin {
   AnimationController _controller;
 
-  List<Animation<int>> _typewriterText = [];
-  List<Animation<double>> _fadeOut = [];
-
+  Animation _typewriterText;
   List<Widget> textWidgetList = [];
+
+  Duration _speed;
+  Duration _pause;
+
+  List<Map> _texts = [];
+
+  int _index;
+
+  bool _isCurrentlyPausing = false;
+
+  Timer _timer;
+
+  int _currentRepeatCount;
 
   @override
   void initState() {
     super.initState();
 
-    if (widget.duration == null) {
-      int totalCharacters = 0;
+    _speed = widget.speed ?? Duration(milliseconds: 30);
+    _pause = widget.pause ?? Duration(milliseconds: 1000);
 
-      for (int i = 0; i < widget.text.length; i++) {
-        totalCharacters += (widget.text[i].length + 8);
+    _index = -1;
+
+    _currentRepeatCount = 0;
+
+    for (int i = 0; i < widget.text.length; i++) {
+      try {
+        if (!widget.text[i].containsKey('text')) throw Error();
+
+        _texts.add({
+          'text': widget.text[i]['text'],
+          'speed': widget.text[i].containsKey('speed')
+              ? widget.text[i]['speed']
+              : _speed,
+          'pause': widget.text[i].containsKey('pause')
+              ? widget.text[i]['pause']
+              : _pause
+        });
+      } catch (e) {
+        _texts.add({'text': widget.text[i], 'speed': _speed, 'pause': _pause});
       }
-
-      _duration = Duration(milliseconds: totalCharacters * 5000 ~/ 15);
-    } else {
-      _duration = widget.duration;
     }
 
-    _controller = new AnimationController(
-      duration: _duration,
-      vsync: this,
-    );
-
-    if (widget.isRepeatingAnimation) {
-      _controller..repeat();
-    } else {
-      _controller.forward();
-    }
-
-    int totalCharacters = 0;
-
-    for (int i = 0; i < widget.text.length; i++) {
-      totalCharacters += (widget.text[i].length + 8);
-    }
-
-    double percentTimeCount = 0.0;
-    for (int i = 0; i < widget.text.length; i++) {
-      double percentTime = (widget.text[i].length + 8) / totalCharacters;
-
-      _typewriterText.add(StepTween(begin: 0, end: widget.text[i].length + 8)
-          .animate(new CurvedAnimation(
-              parent: _controller,
-              curve: Interval(
-                  percentTimeCount, (percentTimeCount + (percentTime * 8 / 10)),
-                  curve: Curves.linear))));
-
-      _fadeOut.add(Tween(begin: 1.0, end: 0.0).animate(new CurvedAnimation(
-          parent: _controller,
-          curve: Interval((percentTimeCount + (percentTime * 9 / 10)),
-              (percentTimeCount + percentTime),
-              curve: Curves.easeIn))));
-
-      percentTimeCount += percentTime;
-    }
+    _nextAnimation();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    if (_controller != null) {
+      _controller
+        ..stop()
+        ..dispose();
+    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    for (int i = 0; i < widget.text.length; i++) {
-      if (i != widget.text.length - 1) {
-        textWidgetList.add(AnimatedBuilder(
-          animation: _controller,
-          builder: (BuildContext context, Widget child) {
-            return Opacity(
-              opacity: _fadeOut[i].value,
-              child: Builder(
-                builder: (BuildContext context) {
-                  String visibleString = widget.text[i];
-                  if (_typewriterText[i].value == 0) {
+    return GestureDetector(
+        onTap: _onTap,
+        child: _isCurrentlyPausing || !_controller.isAnimating
+            ? Text(
+                _texts[_index]['text'],
+                style: widget.textStyle,
+                textAlign: widget.textAlign,
+              )
+            : AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget child) {
+                  String visibleString = _texts[_index]['text'];
+                  if (_typewriterText.value == 0) {
                     visibleString = "";
-                  } else if (_typewriterText[i].value > widget.text[i].length) {
-                    if ((_typewriterText[i].value - widget.text[i].length) %
+                  } else if (_typewriterText.value >
+                      _texts[_index]['text'].length) {
+                    if ((_typewriterText.value -
+                                _texts[_index]['text'].length) %
                             2 ==
                         0) {
-                      visibleString =
-                          widget.text[i].substring(0, widget.text[i].length) +
-                              "_";
+                      visibleString = _texts[_index]['text']
+                              .substring(0, _texts[_index]['text'].length) +
+                          '_';
                     } else {
-                      visibleString =
-                          widget.text[i].substring(0, widget.text[i].length);
+                      visibleString = _texts[_index]['text']
+                          .substring(0, _texts[_index]['text'].length);
                     }
                   } else {
-                    visibleString =
-                        widget.text[i].substring(0, _typewriterText[i].value) +
-                            "_";
+                    visibleString = _texts[_index]['text']
+                            .substring(0, _typewriterText.value) +
+                        '_';
                   }
+
                   return Text(
                     visibleString,
                     style: widget.textStyle,
                     textAlign: widget.textAlign,
                   );
                 },
-              ),
-            );
-          },
-        ));
+              ));
+  }
+
+  void _nextAnimation() {
+    bool isLast = _index == widget.text.length - 1;
+
+    _isCurrentlyPausing = false;
+
+    // Handling onNext callback
+    if (_index > -1) {
+      if (widget.onNext != null) widget.onNext(_index, isLast);
+    }
+
+    if (isLast) {
+      if (widget.isRepeatingAnimation &&
+          (_currentRepeatCount != (widget.totalRepeatCount - 1))) {
+        _index = 0;
+        _currentRepeatCount++;
       } else {
-        if (widget.isRepeatingAnimation) {
-          textWidgetList.add(AnimatedBuilder(
-            animation: _controller,
-            builder: (BuildContext context, Widget child) {
-              return Opacity(
-                opacity: _fadeOut[i].value,
-                child: Builder(
-                  builder: (BuildContext context) {
-                    String visibleString = widget.text[i];
-                    if (_typewriterText[i].value == 0) {
-                      visibleString = "";
-                    } else if (_typewriterText[i].value >
-                        widget.text[i].length) {
-                      if ((_typewriterText[i].value - widget.text[i].length) %
-                              2 ==
-                          0) {
-                        visibleString =
-                            widget.text[i].substring(0, widget.text[i].length) +
-                                "_";
-                      } else {
-                        visibleString =
-                            widget.text[i].substring(0, widget.text[i].length);
-                      }
-                    } else {
-                      visibleString = widget.text[i]
-                              .substring(0, _typewriterText[i].value) +
-                          "_";
-                    }
-                    return Text(
-                      visibleString,
-                      style: widget.textStyle,
-                      textAlign: widget.textAlign,
-                    );
-                  },
-                ),
-              );
-            },
-          ));
-        } else {
-          textWidgetList.add(AnimatedBuilder(
-            animation: _controller,
-            builder: (BuildContext context, Widget child) {
-              return Opacity(
-                opacity: 1,
-                child: Builder(
-                  builder: (BuildContext context) {
-                    String visibleString = widget.text[i];
-                    if (_typewriterText[i].value == 0) {
-                      visibleString = "";
-                    } else if (_typewriterText[i].value >
-                        widget.text[i].length) {
-                      if ((_typewriterText[i].value - widget.text[i].length) %
-                              2 ==
-                          0) {
-                        visibleString =
-                            widget.text[i].substring(0, widget.text[i].length) +
-                                "_";
-                      } else {
-                        visibleString =
-                            widget.text[i].substring(0, widget.text[i].length);
-                      }
-                    } else {
-                      visibleString = widget.text[i]
-                              .substring(0, _typewriterText[i].value) +
-                          "_";
-                    }
-                    return Text(
-                      visibleString,
-                      style: widget.textStyle,
-                      textAlign: widget.textAlign,
-                    );
-                  },
-                ),
-              );
-            },
-          ));
+        if (widget.onFinished != null) widget.onFinished();
+        return;
+      }
+    } else {
+      _index++;
+    }
+
+    if (_controller != null) _controller.dispose();
+
+    setState(() {});
+
+    _controller = new AnimationController(
+      duration: _texts[_index]['speed'] * _texts[_index]['text'].length,
+      vsync: this,
+    );
+
+    _typewriterText =
+        StepTween(begin: 0, end: _texts[_index]['text'].length + 8)
+            .animate(_controller)
+              ..addStatusListener(_animationEndCallback);
+
+    _controller.forward();
+  }
+
+  void _setPause() {
+    bool isLast = _index == widget.text.length - 1;
+
+    _isCurrentlyPausing = true;
+    setState(() {});
+
+    // Handle onNextBeforePause callback
+    if (widget.onNextBeforePause != null)
+      widget.onNextBeforePause(_index, isLast);
+  }
+
+  void _animationEndCallback(state) {
+    if (state == AnimationStatus.completed) {
+      _setPause();
+      _timer = Timer(_texts[_index]['pause'], _nextAnimation);
+    }
+  }
+
+  void _onTap() {
+    int pause;
+    int left;
+
+    if (widget.displayFullTextOnTap) {
+      if (_isCurrentlyPausing) {
+        if (widget.stopPauseOnTap) {
+          _timer?.cancel();
+          _nextAnimation();
         }
+      } else {
+        pause = _texts[_index]['pause'].inMilliseconds;
+        left = _texts[_index]['speed'].inMilliseconds *
+            (_texts[_index]['text'].length - _typewriterText.value);
+
+        _controller.stop();
+
+        _setPause();
+
+        _timer =
+            Timer(Duration(milliseconds: max(pause, left)), _nextAnimation);
       }
     }
 
-    return GestureDetector(
-      onTap: widget.onTap,
-      child: Stack(
-        alignment: widget.alignment,
-        children: textWidgetList,
-      ),
-    );
+    if (widget.onTap != null) {
+      widget.onTap();
+    }
   }
 }
