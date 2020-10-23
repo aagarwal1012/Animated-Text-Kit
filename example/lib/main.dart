f3dfd316c939cc2d027538291e26ff2ef00bff50
Anthony Whitford <anthony@whitford.com>
2020-10-23 05:39:44 -0700

    Revised example to pass pedantic rules.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,14 +4,14 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 void main() => runApp(MyApp());
 
 const List<String> labels = [
-  "Rotate",
-  "Fade",
-  "Typer",
-  "Typewriter",
-  "Scale",
-  "Colorize",
-  "TextLiquidFill",
-  "Wavy Text"
+  'Rotate',
+  'Fade',
+  'Typer',
+  'Typewriter',
+  'Scale',
+  'Colorize',
+  'TextLiquidFill',
+  'Wavy Text'
 ];
 
 class MyApp extends StatefulWidget {
@@ -47,7 +47,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> _textAnimationKit = [
+  final _textAnimationKit = <Widget>[
     ListView(
       scrollDirection: Axis.horizontal,
       children: <Widget>[
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 100.0,
             ),
             Text(
-              "Be",
+              'Be',
               style: TextStyle(fontSize: 43.0),
             ),
             SizedBox(
@@ -68,13 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             RotateAnimatedTextKit(
               onTap: () {
-                print("Tap Event");
+                print('Tap Event');
               },
               isRepeatingAnimation: true,
               totalRepeatCount: 10,
-              text: ["AWESOME", "OPTIMISTIC", "DIFFERENT"],
+              text: ['AWESOME', 'OPTIMISTIC', 'DIFFERENT'],
               // alignment: Alignment(1.0, 0.5),
-              textStyle: TextStyle(fontSize: 40.0, fontFamily: "Horizon"),
+              textStyle: TextStyle(fontSize: 40.0, fontFamily: 'Horizon'),
             ),
           ],
         ),
@@ -82,60 +82,60 @@ class _MyHomePageState extends State<MyHomePage> {
     ),
     FadeAnimatedTextKit(
       onTap: () {
-        print("Tap Event");
+        print('Tap Event');
       },
-      text: ["do IT!", "do it RIGHT!!", "do it RIGHT NOW!!!"],
+      text: ['do IT!', 'do it RIGHT!!', 'do it RIGHT NOW!!!'],
       textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
     ),
     SizedBox(
       width: 250.0,
       child: TyperAnimatedTextKit(
         onTap: () {
-          print("Tap Event");
+          print('Tap Event');
         },
         text: [
-          "It is not enough to do your best,",
-          "you must know what to do,",
-          "and then do your best",
-          "- W.Edwards Deming",
+          'It is not enough to do your best,',
+          'you must know what to do,',
+          'and then do your best',
+          '- W.Edwards Deming',
         ],
-        textStyle: TextStyle(fontSize: 30.0, fontFamily: "Bobbers"),
+        textStyle: TextStyle(fontSize: 30.0, fontFamily: 'Bobbers'),
       ),
     ),
     SizedBox(
       width: 250.0,
       child: TypewriterAnimatedTextKit(
         onTap: () {
-          print("Tap Event");
+          print('Tap Event');
         },
         text: [
-          "Discipline is the best tool",
-          "Design first, then code",
-          "Do not patch bugs out, rewrite them",
-          "Do not test bugs out, design them out",
+          'Discipline is the best tool',
+          'Design first, then code',
+          'Do not patch bugs out, rewrite them',
+          'Do not test bugs out, design them out',
         ],
-        textStyle: TextStyle(fontSize: 30.0, fontFamily: "Agne"),
+        textStyle: TextStyle(fontSize: 30.0, fontFamily: 'Agne'),
       ),
     ),
     ScaleAnimatedTextKit(
       onTap: () {
-        print("Tap Event");
+        print('Tap Event');
       },
-      text: ["Think", "Build", "Ship"],
-      textStyle: TextStyle(fontSize: 70.0, fontFamily: "Canterbury"),
+      text: ['Think', 'Build', 'Ship'],
+      textStyle: TextStyle(fontSize: 70.0, fontFamily: 'Canterbury'),
     ),
 
     /// colors.length >= 2
     ColorizeAnimatedTextKit(
       onTap: () {
-        print("Tap Event");
+        print('Tap Event');
       },
       text: [
-        "Larry Page",
-        "Bill Gates",
-        "Steve Jobs",
+        'Larry Page',
+        'Bill Gates',
+        'Steve Jobs',
       ],
-      textStyle: TextStyle(fontSize: 50.0, fontFamily: "Horizon"),
+      textStyle: TextStyle(fontSize: 50.0, fontFamily: 'Horizon'),
       colors: [
         Colors.purple,
         Colors.blue,
@@ -157,14 +157,14 @@ class _MyHomePageState extends State<MyHomePage> {
     WavyAnimatedTextKit(
       textStyle: TextStyle(fontSize: 20),
       text: [
-        "Hello World",
-        "Look at the waves",
-        "They look so Amazing",
+        'Hello World',
+        'Look at the waves',
+        'They look so Amazing',
       ],
     ),
   ];
 
-  List<Color> _colors = [
+  final _colors = <Color>[
     Colors.orange[800],
     Colors.brown[600],
     Colors.lightGreen[800],
