9fe6c878e044c20789a67dde285196923a603b71
Anthony Whitford <anthony@whitford.com>
2020-12-15 12:18:52 -0800

    Refactored the example app to make it easier to grow.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,43 +3,27 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() => runApp(MyApp());
 
-const List<String> labels = [
-  'Rotate',
-  'Fade',
-  'Typer',
-  'Typewriter',
-  'Scale',
-  'Colorize',
-  'TextLiquidFill',
-  'Wavy Text'
-];
-
 class MyApp extends StatefulWidget {
   /// This widget is the root of your application.
   @override
-  MyAppState createState() {
-    return MyAppState();
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animated Text Kit',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Animated Text Kit'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-
   MyHomePage({
     Key key,
-    this.title,
   }) : super(key: key);
 
   @override
@@ -47,139 +31,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> get _textAnimationKit => <Widget>[
-        ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                SizedBox(
-                  width: 20.0,
-                  height: 100.0,
-                ),
-                Text(
-                  'Be',
-                  style: TextStyle(fontSize: 43.0),
-                ),
-                SizedBox(
-                  width: 20.0,
-                  height: 100.0,
-                ),
-                RotateAnimatedTextKit(
-                  onTap: () => _onTap(),
-                  isRepeatingAnimation: true,
-                  totalRepeatCount: 10,
-                  text: ['AWESOME', 'OPTIMISTIC', 'DIFFERENT'],
-                  // alignment: Alignment(1.0, 0.5),
-                  textStyle: TextStyle(fontSize: 40.0, fontFamily: 'Horizon'),
-                ),
-              ],
-            ),
-          ],
-        ),
-        FadeAnimatedTextKit(
-          onTap: () => _onTap(),
-          text: ['do IT!', 'do it RIGHT!!', 'do it RIGHT NOW!!!'],
-          textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          width: 250.0,
-          child: TyperAnimatedTextKit(
-            onTap: () => _onTap(),
-            text: [
-              'It is not enough to do your best,',
-              'you must know what to do,',
-              'and then do your best',
-              '- W.Edwards Deming',
-            ],
-            textStyle: TextStyle(fontSize: 30.0, fontFamily: 'Bobbers'),
-          ),
-        ),
-        SizedBox(
-          width: 250.0,
-          child: TypewriterAnimatedTextKit(
-            onTap: () => _onTap(),
-            text: [
-              'Discipline is the best tool',
-              'Design first, then code',
-              'Do not patch bugs out, rewrite them',
-              'Do not test bugs out, design them out',
-            ],
-            textStyle: TextStyle(fontSize: 30.0, fontFamily: 'Agne'),
-          ),
-        ),
-        ScaleAnimatedTextKit(
-          onTap: () => _onTap(),
-          text: ['Think', 'Build', 'Ship'],
-          textStyle: TextStyle(fontSize: 70.0, fontFamily: 'Canterbury'),
-        ),
-
-        /// colors.length >= 2
-        ColorizeAnimatedTextKit(
-          onTap: () => _onTap(),
-          text: [
-            'Larry Page',
-            'Bill Gates',
-            'Steve Jobs',
-          ],
-          textStyle: TextStyle(fontSize: 50.0, fontFamily: 'Horizon'),
-          colors: [
-            Colors.purple,
-            Colors.blue,
-            Colors.yellow,
-            Colors.red,
-          ],
-        ),
-
-        Center(
-          child: TextLiquidFill(
-            text: 'LIQUIDY',
-            waveColor: Colors.blueAccent,
-            boxBackgroundColor: Colors.redAccent,
-            textStyle: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
-            boxHeight: 300,
-          ),
-        ),
-
-        WavyAnimatedTextKit(
-          onTap: () => _onTap(),
-          textStyle: TextStyle(fontSize: 20),
-          text: [
-            'Hello World',
-            'Look at the waves',
-            'They look so Amazing',
-          ],
-        ),
-      ];
-
-  final _colors = <Color>[
-    Colors.orange[800],
-    Colors.brown[600],
-    Colors.lightGreen[800],
-    Colors.teal[700],
-    Colors.blue[700],
-    Colors.blueGrey[50],
-    Colors.white,
-    Colors.black87,
-  ];
-
+  List<AnimatedTextExample> _examples;
   int _index = 0;
   int _tapCount = 0;
 
-  void _onTap() {
-    print('Tap Event');
-    setState(() {
-      _tapCount++;
+  @override
+  void initState() {
+    super.initState();
+    _examples = animatedTextExamples(onTap: () {
+      print('Tap Event');
+      setState(() {
+        _tapCount++;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final animatedTextExample = _examples[_index];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          labels[_index],
+          animatedTextExample.label,
           style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
         ),
       ),
@@ -189,8 +63,8 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Container(),
           ),
           Container(
-            decoration: BoxDecoration(color: _colors[_index]),
-            child: Center(child: _textAnimationKit[_index]),
+            decoration: BoxDecoration(color: animatedTextExample.color),
+            child: Center(child: animatedTextExample.child),
             height: 300.0,
             width: 300.0,
           ),
@@ -205,11 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            _index = ++_index % _textAnimationKit.length;
+            _index = ++_index % _examples.length;
             _tapCount = 0;
           });
         },
-        tooltip: 'Increment',
+        tooltip: 'Next',
         child: const Icon(
           Icons.play_circle_filled,
           size: 50.0,
@@ -218,3 +92,193 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class AnimatedTextExample {
+  final String label;
+  final Color color;
+  final Widget child;
+  const AnimatedTextExample({
+    @required this.label,
+    @required this.color,
+    @required this.child,
+  });
+}
+
+List<AnimatedTextExample> animatedTextExamples({VoidCallback onTap}) =>
+    <AnimatedTextExample>[
+      AnimatedTextExample(
+        label: 'Rotate',
+        color: Colors.orange[800],
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const SizedBox(
+                  width: 20.0,
+                  height: 100.0,
+                ),
+                const Text(
+                  'Be',
+                  style: TextStyle(fontSize: 43.0),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                  height: 100.0,
+                ),
+                RotateAnimatedTextKit(
+                  onTap: onTap,
+                  isRepeatingAnimation: true,
+                  totalRepeatCount: 10,
+                  text: ['AWESOME', 'OPTIMISTIC', 'DIFFERENT'],
+                  textStyle: const TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Horizon',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Fade',
+        color: Colors.brown[600],
+        child: FadeAnimatedTextKit(
+          onTap: onTap,
+          text: ['do IT!', 'do it RIGHT!!', 'do it RIGHT NOW!!!'],
+          textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Typer',
+        color: Colors.lightGreen[800],
+        child: SizedBox(
+          width: 250.0,
+          child: TyperAnimatedTextKit(
+            onTap: onTap,
+            text: [
+              'It is not enough to do your best,',
+              'you must know what to do,',
+              'and then do your best',
+              '- W.Edwards Deming',
+            ],
+            textStyle: const TextStyle(
+              fontSize: 30.0,
+              fontFamily: 'Bobbers',
+            ),
+          ),
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Typewriter',
+        color: Colors.teal[700],
+        child: SizedBox(
+          width: 250.0,
+          child: TypewriterAnimatedTextKit(
+            onTap: onTap,
+            text: [
+              'Discipline is the best tool',
+              'Design first, then code',
+              'Do not patch bugs out, rewrite them',
+              'Do not test bugs out, design them out',
+            ],
+            textStyle: const TextStyle(
+              fontSize: 30.0,
+              fontFamily: 'Agne',
+            ),
+          ),
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Scale',
+        color: Colors.blue[700],
+        child: ScaleAnimatedTextKit(
+          onTap: onTap,
+          text: ['Think', 'Build', 'Ship'],
+          textStyle: const TextStyle(
+            fontSize: 70.0,
+            fontFamily: 'Canterbury',
+          ),
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Colorize',
+        color: Colors.blueGrey[50],
+        child: ColorizeAnimatedTextKit(
+          onTap: onTap,
+          text: [
+            'Larry Page',
+            'Bill Gates',
+            'Steve Jobs',
+          ],
+          textStyle: const TextStyle(
+            fontSize: 50.0,
+            fontFamily: 'Horizon',
+          ),
+          colors: [
+            Colors.purple,
+            Colors.blue,
+            Colors.yellow,
+            Colors.red,
+          ],
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'TextLiquidFill',
+        color: Colors.white,
+        child: TextLiquidFill(
+          text: 'LIQUIDY',
+          waveColor: Colors.blueAccent,
+          boxBackgroundColor: Colors.redAccent,
+          textStyle: const TextStyle(
+            fontSize: 70,
+            fontWeight: FontWeight.bold,
+          ),
+          boxHeight: 300,
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Wavy Text',
+        color: Colors.black87,
+        child: WavyAnimatedTextKit(
+          onTap: onTap,
+          textStyle: const TextStyle(fontSize: 20),
+          text: [
+            'Hello World',
+            'Look at the waves',
+            'They look so Amazing',
+          ],
+        ),
+      ),
+      AnimatedTextExample(
+        label: 'Combination',
+        color: Colors.pink,
+        child: AnimatedTextKit(
+          onTap: onTap,
+          animatedTexts: [
+            FadeAnimatedText(
+              'Fade First',
+              textStyle: const TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ScaleAnimatedText(
+              'Then Scale',
+              textStyle: const TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            WavyAnimatedText(
+              'Awesome',
+              textStyle: const TextStyle(
+                fontSize: 48.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    ];
